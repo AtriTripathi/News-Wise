79df59c60ad2225d4745e666b18602998dceb4e9
Atri Tripathi <atri@Atris-MacBook-Air.local>
2021-01-25 04:19:28 +0530

    First commit

---
+++ app/src/main/java/com/atritripathi/newswise/ui/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.atritripathi.newswise.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.atritripathi.newswise.R
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+
+}
---
+++ app/src/main/java/com/atritripathi/newswise/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.atritripathi.newswise.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.atritripathi.newswise.R
+
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+
+}
